8fb7fa78abb85060e38c23f865c29702d015b508
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2024-01-18 18:13:07 +0530

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from '../features/counter/usersReducer';
-
-export const store = configureStore({
-  reducer: {
-    usersReducer
-  },
-});
